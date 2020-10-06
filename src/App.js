a71c256b78c15a746ae5dca80a1a210dd581bec0
alcimar-filho <alcimar.filho@outlook.com>
2020-10-06 12:19:26 -0400

    projeto completo do primeiro curso

--- src/App.js
+++ src/App.js
@@ -8,20 +8,21 @@ class App extends Component {
 
   constructor(){
     super();
-    this.notas = [1,1];
+    this.state = { notas: []};
   }
 
   criarNota(titulo, texto){
-    console.log('nova nota criada: ' + titulo + " - " + texto);
     const novaNota = {titulo,texto};
-    this.notas.push(novaNota);
+    const novoArrayDeNotas = [...this.state.notas,novaNota];
+    const novoEstado = {notas: novoArrayDeNotas}
+    this.setState(novoEstado);
   }
 
   render() {
     return (
       <section className="conteudo">
         <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
-        <ListaDeNotas notas={this.notas}/>
+        <ListaDeNotas notas={this.state.notas}/>
       </section>
     );
   }
